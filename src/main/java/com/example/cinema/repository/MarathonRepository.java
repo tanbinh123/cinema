93b7c51f600bbf8815ae015d012265359157a342
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-10-19 15:53:53 +0200

    add MarathonRepository and PosterRepository

---
+++ src/main/java/com/example/cinema/repository/MarathonRepository.java
@@ -0,0 +1,7 @@
+package com.example.cinema.repository;
+
+import com.example.cinema.domain.Marathon;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MarathonRepository extends CrudRepository<Marathon, Long> {
+}
