62abb313cabd99127d9911a7858eaaedd7f8490c
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-10-20 15:50:47 +0200

    create Movie controller

--- src/main/java/com/example/cinema/controller/MovieController.java
+++ src/main/java/com/example/cinema/controller/MovieController.java
@@ -1,5 +1,39 @@
 package com.example.cinema.controller;
 
+import com.example.cinema.domain.Movie;
+import com.example.cinema.service.MovieService;
+import lombok.AllArgsConstructor;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/cinema/movie")
+@AllArgsConstructor
 public class MovieController {
+
+    private MovieService movieService;
+
+    @GetMapping("")
+    public Stream<Movie> getAllMovies(){
+        return movieService.getAllMovies(Pageable.unpaged()).get();
+    }
+    @GetMapping("/sorted")
+    public Stream<Movie> getAllMoviesSorted(){
+        return movieService.getAllMovies(PageRequest.of(1,3, Sort.by("title"))).get();
+    }
+
+    @PostMapping("/add")
+    public Movie addMovie(@RequestBody Movie movie){
+        Long id = movieService.createMovie(movie.getTitle(), movie.getCategory(), movie.getLength(), movie.getDescription(), movie.getRequiredAge(),null );
+        movie.setId(id);
+        return movie;
+    }
+
+
+
 }
