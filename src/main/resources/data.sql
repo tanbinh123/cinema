96f039b1ec7befd92bdf548229ad1af3c6ab969a
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-10-20 11:53:35 +0200

    correct data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,29 +1,27 @@
-insert into room (id, name, capacity, description) values (1, 'Sala 1', 130, '10 rzedow po 13 miejsc');
-insert into room (id, name, capacity, description) values (2, 'Sala 2', 121, '11 rzedow po 11 miejsc');
-insert into room (id, name, capacity, description) values (3, 'Sala 3', 150, '10 rzedow po 15 miejsc');
-insert into room (id, name, capacity, description) values (4, 'Sala 4', 144, '12 rzedow po 12 miejsc');
+INSERT INTO room (id,name ,capacity,description) VALUES (1,'Sala 1', 130, '10 rzędów po 13 miejsc');
+INSERT INTO room (id,name ,capacity,description) VALUES (2,'Sala 2', 121, '11 rzędów po 11 miejsc');
+INSERT INTO room (id,name ,capacity,description) VALUES (3,'Sala 3', 150, '10 rzędów po 15 miejsc');
+INSERT INTO room (id,name ,capacity,description) VALUES (4,'Sala 4', 144, '12 rzędów po 12 miejsc');
 
+INSERT INTO movie (id,title,category, length, description, required_age) VALUES (5,'After','ROMANTIC',106,'Ekranizacja betsellerowej powieści..',18);
+INSERT INTO movie (id,title,category, length, description, required_age) VALUES (6,'Avengers: Koniec Gry','ACTION',182,'Połowa życia w kosmosie..',15);
+INSERT INTO movie (id,title,category, length, description, required_age) VALUES (7,'Green Book','COMEDY',130,'Tony, drobny cwaniaczek z Bronxu..',15);
+INSERT INTO movie (id,title,category, length, description, required_age) VALUES (8,'Hellboy','ACTION',121,'Kultowy bohater z piekła rodem..',18);
+INSERT INTO movie (id,title,category, length, description, required_age) VALUES (9,'Impostor','HORROR',90,'Sarah, chąc ucieć przed przeszłością..',18);
+INSERT INTO movie (id,title,category, length, description, required_age) VALUES (10,'Przemytnik','DRAMA',116,'Eastwood wciela się w rolę Earla Stone`a..',15);
+INSERT INTO movie (id,title,category, length, description, required_age) VALUES (11,'Niedobrani','COMEDY',121,'Fred nie jest typem amanta..',15);
 
-insert into movie (id, title, category, length, description, required_Age) values (5, 'After', 'ROMANTIC', 106,'Ekranizacja bestsellera', 16);
-insert into movie (id, title, category, length, description, required_Age) values (6, 'Avengers', 'ACTION', 182,'Ekranizacja bestsellera', 16);
-insert into movie (id, title, category, length, description, required_Age) values (7, 'Green Book', 'COMEDY', 116,'Ekranizacja bestsellera', 16);
-insert into movie (id, title, category, length, description, required_Age) values (8, 'Hellboy gry', 'HORROR', 186,'Ekranizacja bestsellera', 16);
-insert into movie (id, title, category, length, description, required_Age) values (9, 'Przemytnik', 'ROMANTIC', 106,'Ekranizacja bestsellera', 16);
-insert into movie (id, title, category, length, description, required_Age) values (10, 'Niedobrani', 'ROMANTIC', 106,'Ekranizacja bestsellera', 16);
-insert into movie (id, title, category, length, description, required_Age) values (11, 'After 2', 'ROMANTIC', 106,'Ekranizacja bestsellera', 16);
+INSERT INTO poster (id,movie_id,file_path) VALUES (12, 5, '/images/posterHellboy.png');
+INSERT INTO poster (id,movie_id,file_path) VALUES (13, 9, '/images/posterImpostor.png');
+INSERT INTO poster (id,movie_id,file_path) VALUES (14, 10, '/images/posterPrzemytnik.png');
 
+INSERT INTO session (id, movie_id,room_id,start_time) VALUES (15,8,2,TIMESTAMP '2019-10-21 12:00:00');
+INSERT INTO session (id, movie_id,room_id,start_time) VALUES (16,8,1,TIMESTAMP '2019-10-21 15:00:00');
+INSERT INTO session (id, movie_id,room_id,start_time) VALUES (17,10,3,TIMESTAMP '2019-10-21 18:00:00');
 
-insert into poster (id, movie_id, file_path) values (12, 8, '/images/poster1.png');
-insert into poster (id, movie_id, file_path) values (13, 9, '/images/poster2.png');
-insert into poster (id, movie_id, file_path) values (14, 10, '/images/poster3.png');
+INSERT INTO ticket (id, session_id, seat, price) VALUES (18,15,'r10m2',21.00);
+INSERT INTO ticket (id, session_id, seat, price) VALUES (19,15,'r8m1',21.00);
+INSERT INTO ticket (id, session_id, seat, price) VALUES (20,15,'r8m2',18.00);
+INSERT INTO ticket (id, session_id, seat, price) VALUES (21,15,'r5m11',18.00);
 
-insert into session(id, movie_id, room_id, start_time) values (15,8,2, TIMESTAMP '2019-05-21 12:00:00');
-insert into session(id, movie_id, room_id, start_time) values (16,8,1, TIMESTAMP '2019-05-22 12:00:00');
-insert into session(id, movie_id, room_id, start_time) values (17,10,3, TIMESTAMP '2019-05-23 12:00:00');
-
-insert into ticket (id, session_id, seat, price) values (18,15,'r10m3', 21.00);
-insert into ticket (id, session_id, seat, price) values (19,15,'r11m3', 21.00);
-insert into ticket (id, session_id, seat, price) values (20,15,'r10m2', 18.00);
-insert into ticket (id, session_id, seat, price) values (21,15,'r07m3', 18.00);
-
-select setval('public.hibernate_sequence', 21, true);
+SELECT setval('public.hibernate_sequence',21,true);
